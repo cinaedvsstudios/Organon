7348284fe893e428448e3785f7e9256ee5cadb9e
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-07 07:31:32 +0200

    Restore minimal syntactically complete exporter

--- tools/audio-timeline/js/exporter.js
+++ tools/audio-timeline/js/exporter.js
@@ -3,11 +3,11 @@ import { audioCtx, masterMixer } from './audio-mixer.js';
 const masterCanvas = document.getElementById('master-canvas');
 let mediaRecorder = null;
 let recordedChunks = [];
-let canvasStream = null;
-let audioDestination = null;
-let masterStream = null;
 
-function cleanupStreams() {
-  if (masterStream) masterStream.getTracks().forEach(track => track.stop());
-  if (canvasStream) canvasStream.getTracks().forEach(track => track.stop());
-  if (audioDestination) master
+export function startExport(format) {
+  recordedChunks = [];
+  const canvasStream = masterCanvas.captureStream(30);
+  const audioDestination = audioCtx.createMediaStreamDestination();
+  masterMixer.connect(audioDestination);
+  const tracks = [
+    ...canvasStream
