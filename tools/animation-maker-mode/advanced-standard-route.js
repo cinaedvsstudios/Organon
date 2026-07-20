b36e3f315266b26c068257efb0ffbbb38cad594c
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-20 17:53:47 +0200

    Route Advanced Mode back to Animation Maker v0.08

--- tools/animation-maker-mode/advanced-standard-route.js
+++ tools/animation-maker-mode/advanced-standard-route.js
@@ -5,6 +5,6 @@
   button.addEventListener('click', (event) => {
     event.preventDefault();
     event.stopImmediatePropagation();
-    location.href = '../animation-maker-standard/index.html?v=0.07';
+    location.href = '../animation-maker-standard/index.html?v=0.08';
   }, true);
 })();
