84bbc9a299c25a2a4ff4a2332ada0deb4867821e
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-20 06:59:19 +0200

    Point Advanced Animation Maker back to Standard v0.07

--- tools/animation-maker-mode/advanced-standard-route.js
+++ tools/animation-maker-mode/advanced-standard-route.js
@@ -5,6 +5,6 @@
   button.addEventListener('click', (event) => {
     event.preventDefault();
     event.stopImmediatePropagation();
-    location.href = '../animation-maker-standard/index.html?v=0.06';
+    location.href = '../animation-maker-standard/index.html?v=0.07';
   }, true);
 })();
