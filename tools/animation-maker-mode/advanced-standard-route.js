ceeec0c0e302d6085a808f5fa64f7ca939f44b02
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-07-20 04:36:58 +0200

    Return Advanced Animation Maker to frame calculator build

--- tools/animation-maker-mode/advanced-standard-route.js
+++ tools/animation-maker-mode/advanced-standard-route.js
@@ -5,6 +5,6 @@
   button.addEventListener('click', (event) => {
     event.preventDefault();
     event.stopImmediatePropagation();
-    location.href = '../animation-maker-standard/index.html?v=0.04';
+    location.href = '../animation-maker-standard/index.html?v=0.05';
   }, true);
 })();
