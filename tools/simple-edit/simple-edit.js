85a5adf98d327839cc170e24d4cae00035aa387a
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-08-25 13:39:00 +0200

    Load AudioTSM stretch patch v0.19

--- tools/simple-edit/simple-edit.js
+++ tools/simple-edit/simple-edit.js
@@ -14,7 +14,8 @@
     "./simple-edit-effects-library.js?v=0.15",
     "./simple-edit-effects-local.js?v=0.16",
     "./simple-edit-echo-settings.js?v=0.17",
-    "./simple-edit-asset-visibility-fix.js?v=0.18"
+    "./simple-edit-asset-visibility-fix.js?v=0.18",
+    "./simple-edit-stretch-audiotsm.js?v=0.19"
   ];
   for (const source of files) {
     await new Promise((resolve, reject) => {
