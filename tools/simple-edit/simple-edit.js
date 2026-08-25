0e454a6ef5ec644af1323d816486c3d6feaa73e8
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-08-25 13:21:44 +0200

    Load Simple Edit echo settings v0.17

--- tools/simple-edit/simple-edit.js
+++ tools/simple-edit/simple-edit.js
@@ -12,7 +12,8 @@
     "./simple-edit-phase3.js?v=0.13",
     "./simple-edit-layout-fix.js?v=0.14",
     "./simple-edit-effects-library.js?v=0.15",
-    "./simple-edit-effects-local.js?v=0.16"
+    "./simple-edit-effects-local.js?v=0.16",
+    "./simple-edit-echo-settings.js?v=0.17"
   ];
   for (const source of files) {
     await new Promise((resolve, reject) => {
