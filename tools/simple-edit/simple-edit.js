5990b868e4d8919d1f4ecd9662ac6c0f5760e512
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-08-25 15:21:20 +0200

    Load Simple Edit normalize v0.21

--- tools/simple-edit/simple-edit.js
+++ tools/simple-edit/simple-edit.js
@@ -16,7 +16,8 @@
     "./simple-edit-echo-settings.js?v=0.17",
     "./simple-edit-asset-visibility-fix.js?v=0.18",
     "./simple-edit-stretch-audiotsm.js?v=0.19",
-    "./simple-edit-fade-handles.js?v=0.20"
+    "./simple-edit-fade-handles.js?v=0.20",
+    "./simple-edit-normalize.js?v=0.21"
   ];
   for (const source of files) {
     await new Promise((resolve, reject) => {
