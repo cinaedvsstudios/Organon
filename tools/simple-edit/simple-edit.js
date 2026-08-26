6a3126a112c8cea75179a9464e8f3d934fab5b56
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-08-26 03:27:34 +0200

    Bump ORGAVOX loader to v0.53 view restore

--- tools/simple-edit/simple-edit.js
+++ tools/simple-edit/simple-edit.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (async () => {
-  const VERSION = "v0.52";
+  const VERSION = "v0.53";
   const REMOTE_SOUND_FX = "https://raw.githubusercontent.com/rse/soundfx/master/soundfx.d/";
   const LOCAL_SOUND_FX = "./soundeffects/";
   window.ORGAVOX_VERSION = VERSION;
@@ -116,6 +116,7 @@
       window.orgavoxApplyFinalCleanup?.();
       window.orgavoxApplyMenuCleanup?.();
       window.orgavoxRefreshV052Interactions?.();
+      window.orgavoxRestoreViewMenu?.();
       ensureLeadingTransportDivider();
 
       const effectsLibrary = document.querySelector(".effects-library-button") || [...document.querySelectorAll("button")].find((button) => /effects library/i.test(button.textContent || ""));
@@ -141,7 +142,7 @@
     "./simple-edit-stretch-audiotsm.js?v=0.19", "./simple-edit-fade-handles.js?v=0.20", "./simple-edit-normalize.js?v=0.21", "./simple-edit-transpose-engine.js?v=0.26", "./simple-edit-transpose.js?v=0.26",
     "./simple-edit-eq-engine.js?v=0.28", "./simple-edit-eq.js?v=0.28", "./simple-edit-drive-engine.js?v=0.29", "./simple-edit-drive.js?v=0.29", "./simple-edit-dynamics-engine.js?v=0.30", "./simple-edit-dynamics.js?v=0.30",
     "./simple-edit-stereo-engine.js?v=0.35", "./simple-edit-stereo.js?v=0.35", "./simple-edit-lofi-engine.js?v=0.37", "./simple-edit-lofi.js?v=0.37", "./simple-edit-render-tools-engine.js?v=0.38", "./simple-edit-render-tools.js?v=0.46",
-    "./simple-edit-analysis.js?v=0.40", "./simple-edit-project.js?v=0.49", "./simple-edit-markers.js?v=0.43", "./simple-edit-build1.js?v=0.44", "./simple-edit-track-tools.js?v=0.45", "./simple-edit-clip-menu.js?v=0.46", "./simple-edit-snap-tools.js?v=0.47", "./simple-edit-library-tools.js?v=0.48", "./simple-edit-build6.js?v=0.49", "./simple-edit-cleanup.js?v=0.50", "./simple-edit-menus-v051.js?v=0.51", "./simple-edit-v052-interactions.js?v=0.52"
+    "./simple-edit-analysis.js?v=0.40", "./simple-edit-project.js?v=0.49", "./simple-edit-markers.js?v=0.43", "./simple-edit-build1.js?v=0.44", "./simple-edit-track-tools.js?v=0.45", "./simple-edit-clip-menu.js?v=0.46", "./simple-edit-snap-tools.js?v=0.47", "./simple-edit-library-tools.js?v=0.48", "./simple-edit-build6.js?v=0.49", "./simple-edit-cleanup.js?v=0.50", "./simple-edit-menus-v051.js?v=0.51", "./simple-edit-v052-interactions.js?v=0.52", "./simple-edit-v053-view-restore.js?v=0.53"
   ];
 
   for (const source of files) {
